f08e68b537f32392137cd9d674debf13717784f3
7cm-diameter <from.this.ridiculous.loop@gmail.com>
2021-12-18 13:12:19 +0900

    WIP: Add a mrthod to adjust the display format

--- src/app.rs
+++ src/app.rs
@@ -1,5 +1,6 @@
 use dropbox_sdk::{
   default_client::UserAuthDefaultClient,
+  files::Metadata as DBXSDKMetadata,
   oauth2::{get_auth_from_env_or_prompt, Authorization},
 };
 use std::env::{var, VarError};
@@ -20,5 +21,26 @@ impl App {
     };
     let client = UserAuthDefaultClient::new(auth);
     App { client }
+  }
+}
+
+pub struct Metadata {
+  metadata: DBXSDKMetadata,
+}
+
+impl Metadata {
+  pub fn from(metadata: DBXSDKMetadata) -> Self {
+    Metadata { metadata: metadata }
+  }
+}
+
+// TODO: show file size and modifed time
+impl ToString for Metadata {
+  fn to_string(&self) -> String {
+    match &self.metadata {
+      DBXSDKMetadata::File(f) => f.path_display.to_owned().unwrap(),
+      DBXSDKMetadata::Folder(f) => f.path_display.to_owned().unwrap(),
+      DBXSDKMetadata::Deleted(f) => f.path_display.to_owned().unwrap(),
+    }
   }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
-use boxy::app::App;
+use boxy::app::{App, Metadata};
 use boxy::command::ls::ls;
-use dropbox_sdk::files::Metadata;
 
 fn main() {
   let app = App::new();
   let ret = ls(&app.client, None, None);
-  ret.iter().for_each(|f| match f {
-    Metadata::File(f) => println!("{:?}", &f.name),
-    Metadata::Folder(f) => println!("{:?}", &f.name),
-    Metadata::Deleted(f) => println!("{:?}", &f.name),
+  ret.iter().for_each(|f| {
+    let metadata = Metadata::from(f.to_owned());
+    println!("{:?}", metadata.to_string());
   })
 }
